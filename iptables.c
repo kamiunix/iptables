9a4cde65fcc765f47b9012b8b8f6395eba058bfa
sam <sam.champagne@dal.ca>
2020-07-11 19:10:53 -0300

    fixed list_rules bug

--- iptables.c
+++ iptables.c
@@ -141,12 +141,11 @@ static void print_rule(const struct ipt_entry *e,
 	printf("\n");
 }
 
-static void list_rules() {
+static void list_rules(const char *table) {
 	struct xtc_handle *h;   
 	const struct ipt_entry *e;
-	const char *table = "filter";
-	const char *chain = NULL;
 	const int counters = 1;
+	const char *chain = NULL;
 	h = iptc_init(table);   
 
 	if (!h) {
@@ -224,11 +223,149 @@ out:
 	return ret;
 }
 
+static int replace_rule(const char *table, 
+		const char *chain, 
+		unsigned int src, 
+		int inverted_src, 
+		unsigned int dest, 
+		int inverted_dest, 
+		const char *target,
+		unsigned int rulenum)
+{
+	struct {       
+		struct ipt_entry entry;
+		struct xt_standard_target target;
+	} entry;  
+	struct xtc_handle *h;   
+	int ret = 1;    
+
+	h = iptc_init(table);   
+	if (!h) {       
+		fprintf(stderr, "Could not init IPTC library: %s\n", iptc_strerror(errno));       
+		goto out;     
+	}
+
+	memset (&entry, 0, sizeof (entry));
+
+	/* target */
+	entry.target.target.u.user.target_size = XT_ALIGN (sizeof (struct xt_standard_target));
+	strncpy (entry.target.target.u.user.name, target, sizeof (entry.target.target.u.user.name));
+
+	/* entry */
+	entry.entry.target_offset = sizeof (struct ipt_entry);
+	entry.entry.next_offset = entry.entry.target_offset + entry.target.target.u.user.target_size;
+
+	if (src) {
+		entry.entry.ip.src.s_addr = src;
+		entry.entry.ip.smsk.s_addr = 0xFFFFFFFF;
+		if (inverted_src) {
+			entry.entry.ip.invflags |= IPT_INV_SRCIP;
+		}
+	}
+	if (dest) {
+		entry.entry.ip.dst.s_addr  = dest;
+		entry.entry.ip.dmsk.s_addr = 0xFFFFFFFF;
+		if (inverted_dest)
+			entry.entry.ip.invflags |= IPT_INV_DSTIP;
+	}
+
+	if (!iptc_replace_entry (chain, (struct ipt_entry *) &entry, rulenum, h)) {
+		fprintf (stderr, "Could not insert a rule in iptables (table %s) at (rulenum : %u) %s\n", table, rulenum, iptc_strerror (errno));
+		goto out;
+	}
+
+	if (!iptc_commit (h)) {
+		fprintf (stderr, "Could not commit changes in iptables (table %s): %s\n", table, iptc_strerror (errno));
+		goto out;
+	}
+
+	ret = 0;
+out:
+	if (h)
+		iptc_free (h);
+
+	return ret;
+
+	
+}
+
+static int delete_rule(const char *table, 
+		const char *chain, 
+		unsigned int rulenum)
+{
+	struct xtc_handle *h;   
+	int ret = 1;    
+
+	h = iptc_init(table);   
+	if (!h) {       
+		fprintf(stderr, "Could not init IPTC library: %s\n", iptc_strerror(errno));       
+		goto out;     
+	}
+	if (!iptc_delete_num_entry(chain, rulenum, h)) {
+		fprintf(stderr, "Could not init IPTC library: %s\n", iptc_strerror(errno));       
+		goto out;     
+	}
+	if (!iptc_commit (h)) {
+		fprintf (stderr, "Could not commit changes in iptables (table %s): %s\n", table, iptc_strerror (errno));
+		goto out;
+	}
+	ret = 0;
+out:
+	if (h)
+		iptc_free (h);
+
+	return ret;
+}
+
+static int clear_rules(const char *table,
+		const char *chain) 
+{
+	struct xtc_handle *h;   
+	int ret = 1;    
+
+	h = iptc_init(table);   
+	if (!h) {       
+		fprintf(stderr, "Could not init IPTC library: %s\n", iptc_strerror(errno));       
+		goto out;     
+	}
+	if (!iptc_flush_entries(chain, h)) {
+		fprintf(stderr, "Could not init IPTC library: %s\n", iptc_strerror(errno));       
+		goto out;     
+	}
+	if (!iptc_commit (h)) {
+		fprintf (stderr, "Could not commit changes in iptables (table %s): %s\n", table, iptc_strerror (errno));
+		goto out;
+	}
+	ret = 0;
+out:
+	if (h)
+		iptc_free (h);
+
+	return ret;
+}
+
+
 int main(int argc, char **argv) {   
-	unsigned int a, b;   
+	unsigned int a, b, c;   
 	inet_pton(AF_INET, "1.2.3.4", &a);  
 	inet_pton(AF_INET, "4.3.2.1", &b);   
+	inet_pton(AF_INET, "1.1.1.1", &c);   
+
+	clear_rules("filter", "INPUT");
+	list_rules("filter");
+
+	printf("%s\n", "inserting rules");
 	insert_rule("filter", "INPUT", a, 0, b, 1, "DROP");   
-	list_rules();
+	insert_rule("filter", "INPUT", a, 0, c, 1, "DROP");   
+	list_rules("filter");
+
+	printf("%s %u\n", "replacing rule", 1);
+	replace_rule("filter", "INPUT", a, 0, b, 1, "ACCEPT", 1);   
+	list_rules("filter");
+
+	printf("%s %u\n", "deleting rule", 0);
+	delete_rule("filter", "INPUT", 0);
+	list_rules("filter");
+
 	return 0; 
 }
